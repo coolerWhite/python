d7ff3959f235164a28b2b69d5bddea5855e5a7a9
coolerwhite <ronaldinio09@yandex.ru>
2024-06-08 16:40:33 +0300

    chapter5

---
+++ DevOps/chapter5/hello-world/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hellow-world",
+    version="0.0.1",
+    author="Example",
+    url="example.com",
+    description="A hello-world exapmle package",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operation System :: OS Idependent"
+    ],
+)
